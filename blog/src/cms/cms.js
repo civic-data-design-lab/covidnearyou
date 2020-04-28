af6e9606a7ef780ef6b679823c6e91f959c55e88
Innocent Obi <innocentobi@nodenigeria.com>
2020-04-27 22:20:50 -0500

    testing collection for jup v1.7192

--- blog/src/cms/cms.js
+++ blog/src/cms/cms.js
@@ -1,5 +1,8 @@
 import CMS from "netlify-cms-app";
 import IframeCMSWidget from "../components/widgets/iframe";
+import uploadcare from "netlify-cms-media-library-uploadcare";
+
+CMS.registerMediaLibrary(uploadcare);
 
 CMS.registerWidget([IframeCMSWidget.Widget()]);
 
