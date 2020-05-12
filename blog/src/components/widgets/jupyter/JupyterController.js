7d56d04822e29a243e6bf32877fc9a449b3eb910
Innocent Obi <innocentobi@nodenigeria.com>
2020-05-12 12:04:36 -0500

    fixed window on unMounter exception

--- blog/src/components/widgets/jupyter/JupyterController.js
+++ blog/src/components/widgets/jupyter/JupyterController.js
@@ -6,22 +6,26 @@ import { css, cx } from "emotion";
 import { Accordion, Label, Container, Icon } from "semantic-ui-react";
 import styled from "@emotion/styled";
 import theme from "../../../gatsby-plugin-theme-ui";
-import {Input, Dataframe, NotebookStaticOverride, StyleZero, Highlights} from './notebook';
+import {
+  Input,
+  Dataframe,
+  NotebookStaticOverride,
+  StyleZero,
+  Highlights,
+} from "./notebook";
 
 export default class JupyterController extends Component {
   constructor(props) {
     super(props);
     this.state = {
       selectedSnippet: "",
-      colorMode: true
-        ? "light" !== window.localStorage.getItem("theme-ui-color-mode", "light")
-        : false,
+      colorMode: true,
       colorState: lightMode,
       activeIndex: 0,
       theme: {
-        input_prompt: "#000000",//"#303F9F",
-        header: "#000000"
-      }
+        input_prompt: "#000000", //"#303F9F",
+        header: "#000000",
+      },
     };
     this.handleClick = this.handleClick.bind(this);
     this.mutateState = this.mutateState.bind(this);
@@ -29,16 +33,16 @@ export default class JupyterController extends Component {
     this.fetchYamlFile = this.fetchYamlFile.bind(this);
   }
 
-  onLog(e){
+  onLog(e) {
     console.log(e.data);
   }
 
-  async fetchYamlFile(){
+  async fetchYamlFile() {
     fetch(`/data/jupyter-snippet/${this.props.path}`)
       .then((response) => response.text())
       .then((data) => {
         const snippets = yaml.load(data);
-        this.setState({selectedSnippet: snippets[`${this.props.snippet}`]});
+        this.setState({ selectedSnippet: snippets[`${this.props.snippet}`] });
       })
       .catch((error) => console.log(error));
   }
@@ -46,17 +50,22 @@ export default class JupyterController extends Component {
   componentDidMount() {
     console.log("Add listener");
     console.log(Object.keys(window.localStorage));
-    //  window.addEventListener('keyup', this.onLog);
-    window.addEventListener('message', this.mutateState);
+    this.setState({
+      colorMode: true
+        ? "light" !==
+          window.localStorage.getItem("theme-ui-color-mode", "light")
+        : false,
+    });
     this.fetchYamlFile();
-
-
+    //  window.addEventListener('keyup', this.onLog);
+    window.addEventListener("message", this.mutateState);
+    this.fetchYamlFile();
   }
 
   componentWillUnmount() {
     console.log("Remove listener");
-  //  window.removeEventListener('keyup', this.onLog);
-    window.removeEventListener('message', this.mutateState);
+    //  window.removeEventListener('keyup', this.onLog);
+    window.removeEventListener("message", this.mutateState);
   }
 
   componenetDidUpdate(oldProp, newProp) {
@@ -72,78 +81,80 @@ export default class JupyterController extends Component {
 
   mutateState(e) {
     console.log(`[MUTATING]`);
-    if('theme' in e.data){
-      const mode =  e.data.theme;
-    console.log(`[MUTATING]: ${mode}`);
-    if (mode === "light") {
-      this.setState({
-        colorMode: false,
-        colorState: selectMode("rgba(0, 0, 0, 0.2)"),
-        theme: {
-          input_prompt: "#000000",//"#303F9F",
-          header: "#000000"
-        },
-      });
-    } else {
-      this.setState({
-        colorMode: true,
-        colorState: selectMode("rgba(255, 255, 255, 0.2)"),
-        theme: {
-          input_prompt: "#FFFFFF",
-          header: "#FFFFFF"
-        },
-      });
+    if ("theme" in e.data) {
+      const mode = e.data.theme;
+      console.log(`[MUTATING]: ${mode}`);
+      if (mode === "light") {
+        this.setState({
+          colorMode: false,
+          colorState: selectMode("rgba(0, 0, 0, 0.2)"),
+          theme: {
+            input_prompt: "#000000", //"#303F9F",
+            header: "#000000",
+          },
+        });
+      } else {
+        this.setState({
+          colorMode: true,
+          colorState: selectMode("rgba(255, 255, 255, 0.2)"),
+          theme: {
+            input_prompt: "#FFFFFF",
+            header: "#FFFFFF",
+          },
+        });
+      }
     }
-  }
   }
 
   render() {
     return (
       <div key="diev" className={this.state.colorState}>
-      <Body className={'jupyter'} borderColor={this.state.theme}>
-        <Accordion fluid inverted={this.state.colorMode}>
-          <Accordion.Title
-            active={this.state.activeIndex === 0}
-            index={0}
-            onClick={this.handleClick}
-          >
-            <Container>
-              <Icon name="dropdown" />
-              {this.props.label + ' ' + this.state.colorMode}
-            </Container>
-          </Accordion.Title>
-          <Accordion.Content
-            active={this.state.activeIndex === 0}
-          ><Test backgroundColor={'hotpink'} color={this.state.theme}
-            dangerouslySetInnerHTML={{
-              __html: this.state.selectedSnippet,
-            }}
-          ></Test></Accordion.Content>
-        </Accordion>
-      </Body>
+        <Body className={"jupyter"} borderColor={this.state.theme}>
+          <Accordion fluid inverted={this.state.colorMode}>
+            <Accordion.Title
+              active={this.state.activeIndex === 0}
+              index={0}
+              onClick={this.handleClick}
+            >
+              <Container>
+                <Icon name="dropdown" />
+                {this.props.label + " " + this.state.colorMode}
+              </Container>
+            </Accordion.Title>
+            <Accordion.Content active={this.state.activeIndex === 0}>
+              <Test
+                backgroundColor={"hotpink"}
+                color={this.state.theme}
+                dangerouslySetInnerHTML={{
+                  __html: this.state.selectedSnippet,
+                }}
+              ></Test>
+            </Accordion.Content>
+          </Accordion>
+        </Body>
       </div>
     );
   }
 }
 
 const Body = styled.div(
-{
-  border: "1px solid",
-  height: "auto",
-width: "100%",
-padding: "4px 12px 12px 4px",
-borderRadius: "4px"
-},
-props => ({borderColor: props.borderColor.header})
+  {
+    border: "1px solid",
+    height: "auto",
+    width: "100%",
+    padding: "4px 12px 12px 4px",
+    borderRadius: "4px",
+  },
+  (props) => ({ borderColor: props.borderColor.header })
 );
 
 const Test = styled.div(
   Dataframe,
-  NotebookStaticOverride,Highlights,
- props => Input(props),
+  NotebookStaticOverride,
+  Highlights,
+  (props) => Input(props),
   StyleZero
 );
-
 
 const BodyCSS = css`
   /* Hello */
